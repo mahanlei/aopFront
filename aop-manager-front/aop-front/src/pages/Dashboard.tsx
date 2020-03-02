ff714e1a9fcc3d9fd91cce36e86eff13d7bf4941
HaileyMa <1216832052@qq.com>
2020-03-02 19:42:30 +0800

    移出manager项目

--- aop-manager-front/aop-front/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-class Dashboard extends React.Component{
-    render(){
-        return <div>此处会放一个全部的链路图</div>
-    }
-}
-export default Dashboard
--- aop-manager-front/aop-front/src/utils/GlobalConstants.js
+++
@@ -1,3 +0,0 @@
-var serverIP = 'http://112.124.17.107:8082';
-// var serverIP = 'http://172.19.97.49:8080';
-export { serverIP };
